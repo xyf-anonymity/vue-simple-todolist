b518d4a3ea171751271766635ee4f0e79b2596c0
xyf-anonymity <1354630349@qq.com>
2021-02-18 21:03:04 +0800

    update

---
+++ src/components/todo-list.vue
@@ -0,0 +1,50 @@
+<template>
+    <ul class="todo-main">
+        <list-item  
+            v-for="item 
+            in todolist" 
+            :key="item.key" 
+            :item="item"
+        >
+        </list-item>
+    </ul>
+</template>
+
+<script>
+import listItem from './list-item'
+export default {
+    name:'v-list',
+    props:{
+        todolist:Array
+    },
+    data(){
+        return {
+            hightlight:true
+        }
+    },
+    components:{
+        "list-item":listItem
+    },
+    mounted(){
+        console.log(this.todolist)
+    }
+}
+</script>
+
+<style  scoped>
+    .todo-main {
+        margin-left: 0px;
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        padding: 0px;
+    }
+
+    .todo-empty {
+        height: 40px;
+        line-height: 40px;
+        border: 1px solid #ddd;
+        border-radius: 2px;
+        padding-left: 5px;
+        margin-top: 10px;
+    }
+</style>
